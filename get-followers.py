b6e7ad183fb7b3f11f4f0d0830bc01acd59bc646
paytonZ <jlpv_90@hotmail.com>
2014-07-07 21:24:52 +0200

    pinterest working

--- get-followers.py
+++ get-followers.py
@@ -29,7 +29,13 @@ from linkedin import linkedin
 from twython import Twython
 
 
-print "Just a little try"
+print "=========================================================="
+print "Python Script for getting Followers in Social Networks"
+print "The Script is given AS IS, dont blame the dev if it dont work"
+print "If you're not agree , close this script inmediatly"
+print "=========================================================="
+
+
 
 twitter_token = "103836433-NhVUnFa94eVEa1YOdZQkl4qV77NiCfVp6888fYvI"
 twitter_token_secret ="qFvHq82TwQPkGK10yc6jMxqztz1tcBlWPVCKIRDe9BVWb"
@@ -91,7 +97,7 @@ OAuth User Secret:
 5af9fab1-91ad-40f2-865f-9ed0c2f3f961
 
 
-'''
+
 
 linkedin_api_key = "77bnwshr9m4k8m"
 linkedin_api_secret = "quxcGwVh5zjK3rLU"
@@ -103,3 +109,13 @@ authentication.authorization_code = raw_input("Insert here the auth token: ")
 token = authentication.get_access_token()
 application = linkedin.LinkedInApplication(token='AQTPEQE-tb9zYCFwqDK9KAt8oboIPL4A9cGip9LYNxltYXwDFJVCnreE6rJ_GQRxHPXPj4d0dUcR4PqSRjsgudOeZfyPBV9De11Hr1M2bK0YILV-it')
 application.get_company_updates(1035, params={'count': 2})
+'''
+#pinterest_api_token ="MTQzMTU5NDozODY4ODc1NjE2NDcyNTY3NDU6NjU1MzV8MTQwNDc1OTM3NjoyNTkyMDAwLS1iZDM3NzU2NTVlNWViNTc1MjVhMDdkMzc2MjBjMmFmNQ%3D%3D"
+pinterest_api_token = "MTQzMTU5NDozODY4ODc1NjE2NDcyNTY3NDU6NjU1MzV8MTQwNDc1OTM3NjoyNTkyMDAwLS1iZDM3NzU2NTVlNWViNTc1MjVhMDdkMzc2MjBjMmFmNQ=="
+pinterest_url = "https://api.pinterest.com/v3/users/%s/?access_token="
+pinterest_user = "baquia"
+pinterest_final_url = pinterest_url % pinterest_user + pinterest_api_token
+data = json.load(urllib2.urlopen(pinterest_final_url))
+print json.dumps(data['data']['follower_count'])
+
+
