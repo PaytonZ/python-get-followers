1f17619cc865751f621e5269806d213ead0e42cb
Plandevida <plandevida@planes.com>
2014-07-10 19:41:57 +0200

    improvements on listener

--- MainGUi.py
+++ MainGUi.py
@@ -6,11 +6,25 @@ from GuiGenerated import Ui_MainWindow
 from PySide import QtCore, QtGui
 import sys
 
-def listenerSaveButton(stri):
-	if stri != "":
+def listenerClientName(textValue):
+	if textValue != "":
 		mySW.ui.saveBt.setEnabled(True)
+		mySW.ui.pinterestText.setEnabled(True)
+		mySW.ui.googleText.setEnabled(True)
+		mySW.ui.facebookText.setEnabled(True)
+		mySW.ui.linkedinText.setEnabled(True)
+		mySW.ui.youtubeText.setEnabled(True)
+		mySW.ui.twitterText.setEnabled(True)
+		mySW.ui.clientNameText.setEnabled(True)
 	else:
 		mySW.ui.saveBt.setEnabled(False)
+		mySW.ui.pinterestText.setEnabled(False)
+		mySW.ui.googleText.setEnabled(False)
+		mySW.ui.facebookText.setEnabled(False)
+		mySW.ui.linkedinText.setEnabled(False)
+		mySW.ui.youtubeText.setEnabled(False)
+		mySW.ui.twitterText.setEnabled(False)
+		mySW.ui.clientNameText.setEnabled(False)
 
 class ControlMainWindow(QtGui.QMainWindow):
 	def __init__(self, parent=None):
@@ -19,7 +33,7 @@ class ControlMainWindow(QtGui.QMainWindow):
 		self.ui.setupUi(self)
 
 	def customSetUp(self):
-		QtCore.QObject.connect(self.ui.clientNameText, QtCore.SIGNAL("textEdited(QString)"), listenerSaveButton)
+		QtCore.QObject.connect(self.ui.clientNameText, QtCore.SIGNAL("textEdited(QString)"), listenerClientName)
 
 if __name__ == '__main__':
 	app = QtGui.QApplication(sys.argv)
