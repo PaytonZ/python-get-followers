37a66f99c07d88356367f8fd02c78fc92b370768
Plandevida <plandevida@planes.com>
2014-07-10 19:21:44 +0200

    UUUoOOhoUouhHh primer -listener- en esta interfaz demoníaca

--- MainGUi.py
+++ MainGUi.py
@@ -6,14 +6,24 @@ from GuiGenerated import Ui_MainWindow
 from PySide import QtCore, QtGui
 import sys
 
+def listener(stri):
+	if stri != "":
+		mySW.ui.saveBt.setEnabled(True)
+	else:
+		mySW.ui.saveBt.setEnabled(False)
+
 class ControlMainWindow(QtGui.QMainWindow):
 	def __init__(self, parent=None):
 		super(ControlMainWindow, self).__init__(parent)
 		self.ui = Ui_MainWindow()
 		self.ui.setupUi(self)
 
+	def customSetUp(self):
+		QtCore.QObject.connect(self.ui.clientNameText, QtCore.SIGNAL("textEdited(QString)"), listener)
+
 if __name__ == '__main__':
 	app = QtGui.QApplication(sys.argv)
 	mySW = ControlMainWindow()
+	mySW.customSetUp()
 	mySW.show()
 	sys.exit(app.exec_())
